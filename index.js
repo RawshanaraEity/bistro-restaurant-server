31a78a8636dece6299bfc18854078e10fcbb7539
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-11-19 12:41:53 +0600

    added get carts from server

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
     const menuCollection = client.db('bistroDB').collection('menu')
     const reviewCollection = client.db('bistroDB').collection('reviews')
+    const cartCollection = client.db('bistroDB').collection('carts')
 
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
@@ -40,6 +41,19 @@ async function run() {
         res.send(result)
     })
 
+    // carts collection
+    app.get('/carts', async(req, res) =>{
+        const email = req.query.email;
+        const query = {email: email}
+        const result = await cartCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    app.post('/carts', async(req,res) =>{
+        const cartItem = req.body;
+        const result = await cartCollection.insertOne(cartItem);
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -60,3 +74,15 @@ app.get('/', (req,res) =>{
 app.listen(port, () =>{
     console.log(`bistro boss is performing on ${port}`);
 })
+
+/***
+ * ------------------------------
+ *     Naming Convention
+ * ------------------------------
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('/users')
+ * app.put('/users/:id')
+ * app.patch('/users/:id')
+ * app.delete('/users/:id')
+ */
